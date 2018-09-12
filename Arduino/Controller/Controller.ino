0a973b359493ce6352713950c9c5af48f38d6909
Roman Zheludkov <romzhel@mail.ru>
2018-09-12 22:17:57 +0300

    initial work enviroment

---
+++ Arduino/Controller/Controller.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		Controller.ino
+ Created:	12.09.2018 22:08:06
+ Author:	Ray
+*/
+
+#include "lib/SmartHouse.h"
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
---
+++ Arduino/Controller/lib/SmartHouse.h
@@ -0,0 +1,16 @@
+
+
+
+
+
+#ifndef _SMARTHOUSE_h
+#define _SMARTHOUSE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+#endif
